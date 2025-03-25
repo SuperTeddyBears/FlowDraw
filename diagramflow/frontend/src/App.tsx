0d28d7e443d14bb9c1d3ce2f4214bc3ed2f75295
krakowskimateusz <mateuszkrakowski2908@gmail.com>
2025-03-25 11:09:19 +0100

    added icons, text

--- diagramflow/frontend/src/App.tsx
+++ diagramflow/frontend/src/App.tsx
@@ -10,7 +10,7 @@ const SIDEBAR_WIDTH = '0.1';
 function HomePage() {
   return (
     <div className='HomePage'>
-      <div className='Register/LoginButtons'>
+      <div className='RegisterLoginButtons'> 
         <Link to="/register">
           <button>Register</button>
         </Link>
@@ -18,12 +18,52 @@ function HomePage() {
           <button className="Login">Login</button> 
         </Link>
       </div>
-      <div>
-        <h1>Welcome to FlowDraw</h1>
+      <div className="WelcomeMessage">
+        <h1> 
+          <span className="welcome">Welcome to</span> 
+          <span className="flow">flow</span>
+          <span className="draw">draw</span>
+          <span className="dot">.</span>
+        </h1>
         <p>Click below to start creating your diagrams!</p>
         <Link to="/app">
-          <button>Go to App</button>
+          <button className='AppButton'>Go to App</button>
         </Link>
+      </div>
+      <div className="NewSection">
+        <div className="LeftContent">
+          <h1>
+            <span className="flow">flow</span>
+            <span className="draw">draw</span>
+            <span className="dot">.</span>
+          </h1>
+          <div className='FeatureList'>
+            <div className="FeatureItem">
+              <span className="FeatureIcon">✏️</span>
+              <span className="FeatureText">Easy drawing</span>
+            </div>
+            <div className="FeatureItem">
+              <span className="FeatureIcon">🔗</span>
+              <span className="FeatureText">Intuitive connections</span>
+            </div>
+            <div className="FeatureItem">
+              <span className="FeatureIcon">📦</span>
+              <span className="FeatureText">Export and share</span>
+            </div>
+            <div className="FeatureItem">
+              <span className="FeatureIcon">🔒</span>
+              <span className="FeatureText">Secure and private</span>
+            </div>
+          </div>
+        </div>
+          
+        <div className="RightContent">
+          <p>
+            <span className="flow">flow</span>
+            <span className="draw">draw</span>
+            <span className="dot">.</span> is a modern online tool for creating diagrams. With a simple interface, you can quickly visualize your ideas, design business processes, and organize information. Create easy-to-read flowcharts, UML or Network diagrams – all in one place!
+          </p>
+        </div>
       </div>
     </div>
   );
