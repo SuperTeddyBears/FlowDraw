76ffa79f0ca1e2f5edd2f561958f012a95d6644f
Floressek <szymon.florek@student.wat.edu.pl>
2025-04-18 01:34:33 +0200

    Modify settings module and introduce custom User model
    The settings module reference in `manage.py` has been updated for better flexibility. Additionally, we've implemented a custom User model inside the `accounts` application. This User model extends Django's default User model by incorporating new fields such as name, email and image. Now, the primary identifier for a User is their email.

--- diagramflow/backend/apps/views.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import status
-from rest_framework.response import Response
-from rest_framework.views import APIView
-from rest_framework_simplejwt.tokens import RefreshToken
-from django.contrib.auth import get_user_model
-from django.contrib.auth.hashers import make_password
-from google.oauth2 import id_token
-from google.auth.transport import requests
-import os
